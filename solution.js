b8e21ab507af8257b63def5ea7bae6152af09a00
Danica Yener <ny21037@googlemail.com>
2022-07-07 15:28:50 +0200

    array

---
+++ solution.js
@@ -0,0 +1,8 @@
+const myArray = ['Aston Martin Cygnet', 'Renault Avantime', 'Maserati Quattroporte', 'Skoda Superb Combi']
+console.log(myArray);
+
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+myArray.slice(3, 1, 'forth elemnt');
+console.log(myArray);
